e21981ee36427c6afa701f4366139e4549b9ec60
plotJ <98871654+plotJ@users.noreply.github.com>
2024-10-21 00:05:54 +0700

    Fix Prisma configuration for PgBouncer compatibility

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,25 +1,27 @@
-// prisma.ts
-import { PrismaClient } from '@prisma/client'
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client';
 
 declare global {
-  var prisma: PrismaClient | undefined
+  // Ensure global prisma is of type PrismaClient
+  // eslint-disable-next-line no-var, no-unused-vars
+  var prisma: PrismaClient | undefined;
 }
 
-let prisma: PrismaClient
+let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient({
     datasources: {
       db: {
-        url: `${process.env.DATABASE_URL}?pgbouncer=true&connection_limit=1`,
+        url: process.env.DATABASE_URL,
       },
     },
-  })
+  });
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient()
+    global.prisma = new PrismaClient();
   }
-  prisma = global.prisma
+  prisma = global.prisma;
 }
 
-export { prisma }
+export { prisma };
